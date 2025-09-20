2c32f6cef8951f4e55c0beaf9a7d0f555dd4e5e1
abshah2005 <abdullah03350904415@gmail.com>
2025-09-20 23:32:31 +0500

    added another section

--- src/components/LandingHeader.jsx
+++ src/components/LandingHeader.jsx
@@ -1,15 +1,20 @@
 import React from "react";
-import img from "../assets/icon.svg"
+import img from "../assets/icon.svg";
+import { Link } from "react-router-dom";
 
 const LandingHeader = () => {
   return (
     <div className="flex flex-col items-center text-center bg-gray-100 py-12 px-4 lg:px-16">
       <div className="flex items-center justify-center gap-2 p-2">
-  <img className="h-6 w-6" src={img} alt="" />
-  <span className="font-semibold text-sm text-blue-600">Direct Answers</span>
-  <span className="bg-orange-400 rounded-full w-1 h-1"></span>
-  <span className="font-semibold text-sm text-blue-600">Real Connections</span>
-</div>
+        <img className="h-6 w-6" src={img} alt="" />
+        <span className="font-semibold text-sm text-blue-600">
+          Direct Answers
+        </span>
+        <span className="bg-orange-400 rounded-full w-1 h-1"></span>
+        <span className="font-semibold text-sm text-blue-600">
+          Real Connections
+        </span>
+      </div>
       <div>
         <h1 className="text-4xl lg:text-5xl font-bold text-gray-800 mb-4">
           Knowledge doesn’t end at the lecture, call, or meeting
@@ -21,10 +26,15 @@ const LandingHeader = () => {
       </p>
       <div className="flex gap-4 ">
         <button className="bg-[#086BFF] text-white px-6 py-3 rounded-full hover:bg-blue-700">
-          Get Started
+          <Link to={"/signup"}> Get Started</Link>
         </button>
         <button className="border rounded-full border-blue-600 text-blue-600 px-6 py-3  hover:bg-blue-100">
-          How it works
+          <a
+            href="/#how-it-works"
+            className="cursor-pointer hover:text-blue-600"
+          >
+            How it works
+          </a>
         </button>
       </div>
     </div>
