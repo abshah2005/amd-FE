dde7bab2ead6287b2e6fa5ef7186a57132777cca
abshah2005 <abdullah03350904415@gmail.com>
2025-09-20 23:27:35 +0500

    additions

--- src/components/LandingHeader.jsx
+++ src/components/LandingHeader.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import img from "../assets/icon.svg"
 
 const LandingHeader = () => {
   return (
     <div className="flex flex-col items-center text-center bg-gray-100 py-12 px-4 lg:px-16">
+      <div className="flex items-center justify-center gap-2 p-2">
+  <img className="h-6 w-6" src={img} alt="" />
+  <span className="font-semibold text-sm text-blue-600">Direct Answers</span>
+  <span className="bg-orange-400 rounded-full w-1 h-1"></span>
+  <span className="font-semibold text-sm text-blue-600">Real Connections</span>
+</div>
       <div>
         <h1 className="text-4xl lg:text-5xl font-bold text-gray-800 mb-4">
           Knowledge doesn’t end at the lecture, call, or meeting
